f684e7d95a792427a5691d65b12c1fba2e5520ac
Muhammad Bilal Kadri <bilal.kadri@gmail.com>
2022-08-31 12:52:18 +0500

    WAypoint navigation for Mission 1 using dronekit has been achieved, more work to be done!

---
+++ script/waypoint_navigation_mission_01_for_dronekit.py
@@ -0,0 +1,339 @@
+#! /usr/bin/env python
+# Import ROS.
+import rospy
+
+#from __future__ import print_function
+
+# Import the API.
+from dronekit import connect, VehicleMode, LocationGlobalRelative, LocationGlobal, Command
+import time
+import math
+from pymavlink import mavutil
+import numpy as np
+import imutils
+from collections import deque
+from std_msgs.msg import Float32MultiArray
+import sys
+import time
+import rospy
+
+takeoff_alt = 5
+
+def set_destination(lat, lon, alt, wp_index):
+    global vehicle
+
+    print("Moving to Waypoint {0}".format(wp_index))
+    
+    aLocation = LocationGlobalRelative(lat, lon, float(alt))
+    
+    # goto_position_target_global_int(aLocation)
+    vehicle.simple_goto(aLocation)
+    #dist_to_wp = dist_between_global_coordinates(vehicle.location.global_frame, aLocation) 
+    dist_to_wp = get_distance_metres(vehicle.location.global_frame, aLocation)
+    
+
+    while dist_to_wp > 10:
+       
+        dist_to_wp = get_distance_metres(vehicle.location.global_frame, aLocation)
+        print("Distance to Waypoint {0}: {1}".format(wp_index, dist_to_wp))
+        print('Vehicle latitude in global frame:',vehicle.location.global_frame.lat)
+        print('Vehicle longitude in global frame:',vehicle.location.global_frame.lon)
+        # dist_to_wp = dist_between_global_coordinates(vehicle.location.global_frame, aLocation) 
+    
+    print("Reached Waypoint {0}".format(wp_index))
+    time.sleep(1)
+
+def goto_position_target_global_int(aLocation):
+    """
+    Send SET_POSITION_TARGET_GLOBAL_INT command to request the vehicle fly to a specified LocationGlobal.
+
+    For more information see: https://pixhawk.ethz.ch/mavlink/#SET_POSITION_TARGET_GLOBAL_INT
+
+    See the above link for information on the type_mask (0=enable, 1=ignore). 
+    At time of writing, acceleration and yaw bits are ignored.
+    """
+    msg = vehicle.message_factory.set_position_target_global_int_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, # frame
+        0b0000111111111000, # type_mask (only speeds enabled)
+        aLocation.lat*1e7, # lat_int - X Position in WGS84 frame in 1e7 * meters
+        aLocation.lon*1e7, # lon_int - Y Position in WGS84 frame in 1e7 * meters
+        aLocation.alt, # alt - Altitude in meters in AMSL altitude, not WGS84 if absolute or relative, above terrain if GLOBAL_TERRAIN_ALT_INT
+        0, # X velocity in NED frame in m/s
+        0, # Y velocity in NED frame in m/s
+        0, # Z velocity in NED frame in m/s
+        0, 0, 0, # afx, afy, afz acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink) 
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+
+def get_distance_metres(aLocation1, aLocation2):
+    """
+    Returns the ground distance in metres between two LocationGlobal objects.
+    This method is an approximation, and will not be accurate over large distances and close to the 
+    earth's poles. It comes from the ArduPilot test code: 
+    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
+    """
+    dlat = aLocation2.lat - aLocation1.lat
+    dlong = aLocation2.lon - aLocation1.lon
+    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
+
+def dist_between_global_coordinates(aLocation1, aLocation2):
+    # This formula has been copied from HEIFU project's repository. Link: https://gitlab.pdmfc.com/drones/ros1/heifu/-/blob/master/heifu_interface/formulas.py
+    R = 6371e3; #in meters
+    latitude1 = math.radians(aLocation1.lat)
+    latitude2 = math.radians(aLocation2.lat)
+    latitudevariation = math.radians((aLocation2.lat-aLocation1.lat))
+    longitudevariation = math.radians((aLocation2.lon-aLocation1.lon))
+    a = math.sin(latitudevariation/2) * math.sin(latitudevariation/2) + math.cos(latitude1) * math.cos(latitude2) * math.sin(longitudevariation/2) * math.sin(longitudevariation/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    distance = R * c #in meters
+    return distance
+
+def distance_to_current_waypoint():
+    """
+    Gets distance in metres to the current waypoint. 
+    It returns None for the first waypoint (Home location).
+    """
+    nextwaypoint = vehicle.commands.next
+    if nextwaypoint==0:
+        return None
+    missionitem=vehicle.commands[nextwaypoint-1] #commands are zero indexed
+    lat = missionitem.x
+    lon = missionitem.y
+    alt = missionitem.z
+    targetWaypointLocation = LocationGlobalRelative(lat,lon,alt)
+    distancetopoint = get_distance_metres(vehicle.location.global_frame, targetWaypointLocation)
+    return 
+
+def get_location_metres(original_location, dNorth, dEast):
+    """
+    Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the 
+    specified `original_location`. The returned Location has the same `alt` value
+    as `original_location`.
+    The function is useful when you want to move the vehicle around specifying locations relative to 
+    the current vehicle position.
+    The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
+    For more information see:
+    http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
+    """
+    earth_radius=6378137.0 #Radius of "spherical" earth
+    #Coordinate offsets in radians
+    dLat = dNorth/earth_radius
+    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
+
+    #New position in decimal degrees
+    newlat = original_location.lat + (dLat * 180/math.pi)
+    newlon = original_location.lon + (dLon * 180/math.pi)
+    return LocationGlobal(newlat, newlon,original_location.alt)
+
+def adds_square_mission(aLocation, aSize):
+    """
+    Adds a takeoff command and four waypoint commands to the current mission. 
+    The waypoints are positioned to form a square of side length 2*aSize around the specified LocationGlobal (aLocation).
+    The function assumes vehicle.commands matches the vehicle mission state 
+    (you must have called download at least once in the session and after clearing the mission)
+    """	
+
+    cmds = vehicle.commands
+
+    print(" Clear any existing commands")
+    cmds.clear() 
+    
+    print(" Define/add new commands.")
+    # Add new commands. The meaning/order of the parameters is documented in the Command class. 
+     
+    #Add MAV_CMD_NAV_TAKEOFF command. This is ignored if the vehicle is already in the air.
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 0, 10))
+
+    #Define the four MAV_CMD_NAV_WAYPOINT locations and add the commands
+    point1 = get_location_metres(aLocation, aSize, -aSize)
+    point2 = get_location_metres(aLocation, aSize, aSize)
+    point3 = get_location_metres(aLocation, -aSize, aSize)
+    point4 = get_location_metres(aLocation, -aSize, -aSize)
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, point1.lat, point1.lon, 11))
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, point2.lat, point2.lon, 12))
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, point3.lat, point3.lon, 13))
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, point4.lat, point4.lon, 14))
+    #add dummy waypoint "5" at point 4 (lets us know when have reached destination)
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, point4.lat, point4.lon, 14))    
+
+    print(" Upload new commands to vehicle")
+    cmds.upload()
+
+def arm_and_takeoff(aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print("Basic pre-arm checks")
+    # Don't let the user try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+        
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    while not vehicle.armed:      
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command 
+    #  after Vehicle.simple_takeoff will execute immediately).
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)      
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: #Trigger just below target alt.
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+
+def main():
+    # Initializing ROS node.
+    rospy.init_node("Waypoint_Navigation", anonymous=True)
+
+    #------------------------------------------------------
+    #     DRONEKIT API
+    #------------------------------------------------------
+    global vehicle
+
+    # Connect to the Vehicle
+    # connection_string = '/dev/ttyUSB0'
+    connection_string = 'udp:127.0.0.1:14550'
+    print('Connecting to vehicle on: %s' % connection_string)
+    vehicle = connect(connection_string, wait_ready=True, baud=921600)
+    print("Connection Successfully Established!")    
+
+    # wp1 = LocationGlobalRelative(24.7944609, 67.1353376, 5)
+    print("Starting Takeoff")
+    
+    print('Create a new mission (for current location)')
+    adds_square_mission(vehicle.location.global_frame,50)
+
+
+    # From Copter 3.3 you will be able to take off using a mission item. Plane must take off using a mission item (currently).
+    arm_and_takeoff(10)
+
+    print("Starting mission")
+    # Reset mission set to first (0) waypoint
+    vehicle.commands.next=0
+
+    # Set mode to AUTO to start mission
+    vehicle.mode = VehicleMode("AUTO")
+
+
+    # Monitor mission. 
+    # Demonstrates getting and setting the command number 
+    # Uses distance_to_current_waypoint(), a convenience function for finding the 
+    #   distance to the next waypoint.
+
+    while True:
+        nextwaypoint=vehicle.commands.next
+        print('Distance to waypoint (%s): %s' % (nextwaypoint, distance_to_current_waypoint()))
+    
+        if nextwaypoint==3: #Skip to next waypoint
+            print('Skipping to Waypoint 5 when reach waypoint 3')
+            vehicle.commands.next = 5
+        if nextwaypoint==5: #Dummy waypoint - as soon as we reach waypoint 4 this is true and we exit.
+            print("Exit 'standard' mission when start heading to final waypoint (5)")
+            break;
+        time.sleep(1)
+
+    print('Return to launch')
+    vehicle.mode = VehicleMode("RTL")
+
+
+    #Close vehicle object before exiting script
+    print("Close vehicle object")
+    vehicle.close()
+        
+        
+    # arm_and_takeoff(takeoff_alt)
+
+    # print("Starting Python Waypoint Navigation")
+    # set_destination(0, 0, 5, 0)       #Arguments: latitutde, longitude, relative altitude, waypoint number
+    # # vehicle.simple_goto(LocationGlobalRelative(-35.3632188, 149.1658468, 5))
+    # time.sleep(20)
+
+
+ 
+    # Specify control loop rate. We recommend a low frequency to not over load the FCU with messages. Too many messages will cause the drone to be sluggish.
+    rate = rospy.Rate(3)
+
+   
+    # Specify some waypoints
+    # goals = [[0, 0, 3, 0], 
+    #          [40, 0, 3, 0], 
+    #          [50, 1.63, 3, 0],
+    #          [65, 12, 3, 0], 
+    #          [70, 30, 3, 0], 
+    #          [68, 40, 3, 0],
+    #          [57, 55, 3, 0],
+    #          [40, 60, 3, 0],
+    #          [0 , 60, 3, 0],
+    #          [-4.6, 61.3, 3, 0],
+    #          [-7, 65, 3, 0],
+    #          [-7.5, 67.5, 3, 0],
+    #          [-7.47,67.36, 3, 0],
+    #          [-6.5,70.62, 3, 0],
+    #          [-3.66,74, 3, 0],
+    #          [0, 75, 3, 0],
+    #          [3, 74.3, 3, 0],
+    #          [5.8, 72, 3, 0],
+    #          [6.5, 71, 3, 0],
+    #          [6.3, 63.1, 3, 0],
+    #          [3.7, 61.19, 3, 0],
+    #          [0, 60, 3, 0],
+    #          [-40, 60, 3, 0],
+    #          [-55.5, 55.83, 3, 0],
+    #          [-65, 42.2, 3, 0],
+    #          [-70, 30, 3, 0],
+    #          [-66, 15.63, 3, 0],
+    #          [-59, 6.7, 3, 0],
+    #          [-53.17, 3.14, 3, 0],
+    #          [-50.14, 2, 3, 0],
+    #          [-41, 0, 3, 0],
+    #          [0, 0, 3, 0]
+             
+    #          ]
+
+
+
+
+    # #int i
+    # i = 0
+    # count=0
+    
+    # while i < len(goals):
+    #     x=goals[i][0]
+    #     y=goals[i][1]
+    #     z=goals[i][2]
+    #     set_destination(x,y,z,count)
+    #     rate.sleep()
+    #     #if check_waypoint_reached():
+    #     i += 1
+        
+
+    print('Completed all Waypoints! Returning to launch')
+    vehicle.mode = VehicleMode("RTL")
+
+
+    #Close vehicle object before exiting script
+    print("Close vehicle object")
+    vehicle.close()
+    rospy.loginfo("All waypoints reached landing now.")
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
